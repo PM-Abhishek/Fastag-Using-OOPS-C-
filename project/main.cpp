bc03bcc293673a8581b30ecc5c30bcfda6f3a9ad
Abhishek P.M <46991362+PM-Abhishek@users.noreply.github.com>
2021-08-01 01:04:04 +0530

    Add files via upload

---
+++ project/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text = "hello";
+    string input;
+    int value;
+    cout << text << endl;
+    cout << "enter your name: "<<flush;
+    cin >> input;
+    cout << "enter your roll number: "<<flush;
+    cin >> value;
+    cout << "the name you entered is: " << input <<endl;
+    cout << "your roll number is: " << value <<endl;
+
+
+
+
+
+
+
+return 0;
+}
